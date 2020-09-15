e7fd3473feca56ad5dc9f83a9b5227d94fd5df74
Iniesta <mohammad.h.b.7798@gmail.com>
2020-09-15 13:40:05 +0430

    add show similiar posts in my website

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from .models import Comment
 from taggit.models import Tag
+from django.db.models import Count
 
 
 # Create your views here
@@ -32,7 +33,7 @@ def post_list(requset, tag_slug=None):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(requset, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag':tag})
+    return render(requset, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
 
 
 def post_detail(requset, year, month, day, post):
@@ -48,8 +49,14 @@ def post_detail(requset, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).distinct().exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(requset, 'blog/post/detail.html', {'post': post, 'comments': comments,
-                                                     'new_comment': new_comment, 'comment_form': comment_form})
+                                                     'new_comment': new_comment, 'comment_form': comment_form,
+                                                     'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
