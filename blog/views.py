9f9aba195e67af7cd6ea722d3a760ae9723c46ab
Iniesta <mohammad.h.b.7798@gmail.com>
2020-09-23 17:04:25 +0330

    Advanced search

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.core.mail import send_mail
 from .models import Comment
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery,SearchRank,TrigramSimilarity
 
 
 # Create your views here
@@ -89,9 +89,21 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(
-                search = SearchVector('title', 'body'),
-                ).filter(search = query)
+            #Step 1:
+            # results = Post.published.annotate(
+            #     search = SearchVector('title', 'body'),
+            #     ).filter(search = query)
+
+            #Step 2:
+            # search_vector = SearchVector('title', 'body')
+            # search_query = SearchQuery(query)
+            # results = Post.published.annotate(search=search_vector, rank = SearchRank(search_vector,search_query)).filter(search=search_query).order_by('-rank')
+
+            #Step 3:
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(rank = SearchRank(search_vector,search_query)).filter(rank__gte = 0.3).order_by('-rank')
+            
     return render(request,
                   'blog/post/search.html',
                   {'form': form,
